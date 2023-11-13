935607cd908be4f35fe03ffdfbb65a487b4bb7bf
MilanBhanuka <vgmbhanuka@gmail.com>
2023-11-14 03:44:04 +0530

    php & AJAX

---
+++ website06/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Search User</title>
+      <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css">
+
+      <script>
+            function showSuggestion(str){
+                  if(str.length == 0){
+                        document.getElementById('output').innerHTML = '';
+                  } else {
+                        // AJAX REQ
+                        var xmlhttp = new XMLHttpRequest();
+                        xmlhttp.onreadystatechange = function(){
+                              if(this.readyState == 4 && this.status == 200){
+                                    document.getElementById('output').innerHTML = this.responseText;
+                              }
+                        }
+                        xmlhttp.open("GET", "suggest.php?q="+str, true);
+                        xmlhttp.send();
+                  }
+            }
+      </script>  
+                  
+</head>
+<body>
+      <div class="container">
+            <h1>Search User</h1>
+            <form>
+                  Search User: <input type="text" class="form-control" onkeyup="showSuggestion(this.value)">
+            </form>
+            <p>Suggestions: <span id="output" style="font-weight:bold"></span></p>
+      </div>      
+</body>
+</html>
